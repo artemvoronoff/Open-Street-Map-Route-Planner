68f8068a121272d54ce49bf2976f5c2bb2a85e0d
artemvoronoff <80431170+artemvoronoff@users.noreply.github.com>
2021-05-19 09:54:25 -0500

    Update route_planner.cpp

--- route_planner.cpp
+++ route_planner.cpp
@@ -15,13 +15,12 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 
 }
 
-
-// TODO 3: Implement the CalculateHValue method.
-// Tips:
-// - You can use the distance to the end_node for the h value.
+// CalculateHValue method calculates the h value for given nodes.
+// The total distance to the ending node.
 // - Node objects have a distance method to determine the distance to another node.
 
 float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
+    return node->distance(*end_node);
 
 }
 
